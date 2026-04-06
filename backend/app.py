284d2f667881432509526bb36d8c7120124f77e5
AzPepoze <azpepoze@gmail.com>
2026-04-07 04:51:14 +0700

    fix: update allowed origins for CORS configuration

--- backend/app.py
+++ backend/app.py
@@ -9,10 +9,6 @@ from controllers.chat_controller import chat_router
 from controllers.user_controller import user_router
 
 ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://127.0.0.1:5000",
-    "http://localhost:5000",
     "http://127.0.0.1:5001",
     "http://localhost:5001",
     "http://0.0.0.0:5001",
